7cb87526c354de7aee770853596d16b4ec06e2bb
becca <rebecca.kaci@gmail.com>
2021-12-16 15:21:54 +0100

    affichage d'un user

--- frontend/src/contexts/user.js
+++ frontend/src/contexts/user.js
@@ -1,7 +1,5 @@
 import { createContext } from "react"
 
-import { useState } from "react"
-
 const apiUrl = `http://localhost:5000/users`
 
 const UserContext = createContext({})
@@ -13,8 +11,15 @@ const UserContextProvider = ({ children }) => {
         .then(data => data)
     }
 
+    const getUser = slug => {
+        return fetch(`${apiUrl}/${slug}`)
+        .then(response => response.json())
+        .then(data => data)
+    }
+
     const value = {
-        getUsers
+        getUsers,
+        getUser
     }
 
     return (
--- frontend/src/pages/User.js
+++ frontend/src/pages/User.js
@@ -1,11 +1,34 @@
-import React from 'react'
+import { useContext, useEffect, useState } from 'react'
+import { useParams } from 'react-router-dom'
+
+import { UserContext } from '../contexts/user'
+
+import Container from '../Components/Container'
+import Img from '../Components/Img'
 
 const User = () => {
+    const { slug } = useParams()
+    const { getUser } = useContext(UserContext)
+    
+    const[user, setUser] = useState(null)
+    
+    useEffect(() => {
+        getUser(slug)
+        .then(response => setUser(response))
+    },[])
+
+    if(!user){
+        return <p>Loading...</p>
+    }
+
     return (
-        <div>
-            User
-        </div>
+        <Container>
+            <Img height="500px" backgroundImage={user.profile_picture} />
+            <h2>{user.name}</h2>
+            <p>From : {user.city}</p>
+            <p>Password : {user.password}</p>
+        </Container>
     );
 };
 
-export default User;
+export default User
